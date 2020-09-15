5c6288149b07346e37eaf2f311f74e970a972f44
Luis <Oceandepths13@yahoo.com>
2020-09-14 21:55:22 -0400

    created test case with testng

---
+++ src/test/java/DemoTest/DemoTest.java
@@ -0,0 +1,24 @@
+package DemoTest;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+    @Test
+    public void practiceTest() {
+        String url = "https://reqres.in/api/users/2";
+
+        Response response = RestAssured.given().get(url).andReturn();
+        response.getBody().prettyPrint();
+    }
+
+    @Test
+    public void practicetest2() {
+        String url = "https://swapi.dev/api/people/1/";
+        Response response = RestAssured.given().get(url).andReturn();
+        response.getBody().prettyPrint();
+    }
+}
